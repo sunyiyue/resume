0913583753ffd3f8390cabc1be00753c2c4a688e
yueyue <sunyiyue@users.noreply.github.com>
2019-02-26 11:20:35 +0800

    修改
    修改

--- data.php
+++ data.php
@@ -8,16 +8,16 @@ $content = 'MarkDown';
 $data['local'] = 1;
 
 
-if( strlen( $viewpass ) > 0 && trim($_REQUEST['vpass']) != $viewpass )
-{
-	$data['errno'] = '0';
-	$data['show'] = 0;
-	$data['title'] = '';
-	$data['subtitle'] = '';
-	$data['content'] = '';
-}
-else
-{
+// if( strlen( $viewpass ) > 0 && trim($_REQUEST['vpass']) != $viewpass )
+// {
+// 	$data['errno'] = '0';
+// 	$data['show'] = 0;
+// 	$data['title'] = '';
+// 	$data['subtitle'] = '';
+// 	$data['content'] = '';
+// }
+// else
+// {
 	
 	$data['errno'] = '0';
 	$data['show'] = 1;
@@ -25,6 +25,6 @@ else
 	$data['subtitle'] = $subtitle;
 	$data['content'] = $content;
 
-}
+// }
 
 echo json_encode( $data );
